fa863b6d9c27a361a1be61464434069378cd8cc7
Justin Chan <jchan922@gmail.com>
2021-01-16 19:27:34 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import test from './Test';
 
 export default class App extends React.Component {
-	componentDidMount() {
-		test();
-	}
-
 	render() {
-		return <h1>Hello World from React boilerplate</h1>;
+		return <h1>Hello World!</h1>;
 	}
 }
